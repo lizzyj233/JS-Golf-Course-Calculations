dd25bf143b1d77152c4df0c7ed06ed19f8607dd9
LizLoftus <ejoloftus@outlook.com>
2021-05-17 14:19:29 -0400

    added note for future enhancement

--- lookup.js
+++ lookup.js
@@ -1,12 +1,16 @@
 //This file will run through the reference "sheet" to find the amount to add to the existing slope rating
 
 function findSlopeDiff(yardDiff){
-    let high = 8;
-    let low = 0;
+    let high = 8; //increments of 18
+    let low = 0; //increments of 9
     let toAdd = 0;
     
-    while(high <= yardDiff){
-        
+    while(high <= yardDiff){ //yardDiff is the highest number
+        if(yardDiff >= low){
+            
+        }
+    high += 18;
+    low += 9;
     }
 
 }
