99b20b068199de60a5f2f0e3e26a2d4d83507929
LizLoftus <ejoloftus@outlook.com>
2021-05-19 22:59:09 -0400

    swapped value assignments for return

--- scripts/lookup.js
+++ scripts/lookup.js
@@ -1,58 +1,55 @@
 //This file will run through the reference "sheet" to find the amount to add to the existing slope rating
 //This is not very efficient, I realize -- I will work to improve this into a loop instead of so many elif statements.
 
-function findSlopeDiff(yardDiff){
-    let toAdd = 0;
-    
+function findSlopeDiff(yardDiff){    
     if(yardDiff <= 44){
-        toAdd = 0;
+        return 0;
     }
     else if(yardDiff > 44 && yardDiff <= 134){
-        toAdd = 1;
+        return 1;
     }
     else if(yardDiff > 134 && yardDiff <= 224){
-        toAdd = 2;
+        return 2;
     }
     else if(yardDiff > 224 && yardDiff <= 296){
-        toAdd = 3;
+        return 3;
     }
     else if(yardDiff > 296 && yardDiff <= 386){
-        toAdd = 4;
+        return 4;
     }
     else if(yardDiff > 386 && yardDiff <= 476){
-        toAdd = 5;
+        return 5;
     }
     else if(yardDiff > 476 && yardDiff <= 548){
-        toAdd = 6;
+        return 6;
     }
     else if(yardDiff > 548 && yardDiff <= 638){
-        toAdd = 7;
+        return 7;
     }
     else if(yardDiff > 638 && yardDiff <= 728){
-        toAdd = 8;
+        return 8;
     }
     else if(yardDiff > 728 && yardDiff <= 800){
-        toAdd = 9;
+        return 9;
     }
     else if(yardDiff > 800 && yardDiff <= 890){
-        toAdd = 10;
+        return 10;
     }
     else if(yardDiff > 890 && yardDiff <= 980){
-        toAdd = 11;
+        return 11;
     }
     else if(yardDiff > 980 && yardDiff <= 1052){
-        toAdd = 12;
+        return 12;
     }
     else if(yardDiff > 1052 && yardDiff <= 1142){
-        toAdd = 13;
+        return 13;
     }
     else if(yardDiff > 1142 && yardDiff <= 1232){
-        toAdd = 14;
+        return 14;
     }
     else{
-        toAdd = 15;
+        return 15;
     }
-    return toAdd;
 }
 
 function findCRDiff(yardDiff){
